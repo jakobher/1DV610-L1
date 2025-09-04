4bfbc6f97a81f14cf051ae71ae16b69c8bbf6ec1
Jakob Hermansson <jh225ud@student.lnu.se>
2025-09-04 12:54:23 +0200

    Initial commit - L1

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+function greeting() {
+    const name = document.getElementById("nameInput").value
+    document.getElementById("greeting").innerHTML = "Hello there " + name + "! " + "Did you know that " + randomFact()
+    document.getElementById("text").innerHTML = "Atleast you do now 😎"
+}
+
+function randomFact() {
+    const facts = [
+    "it took the creator of the Rubik’s Cube one month to solve the cube after he created it.",
+    "a crocodile cannot stick its tongue out.",
+    "it is physically impossible for pigs to look up into the sky.",
+    "a cat has 32 muscles in each ear.",
+    "giraffes are 30 times more likely to get hit by lightning than people."
+    ]
+    return facts[Math.floor(Math.random() * facts.length)]
+} 
